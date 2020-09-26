f9761de4c12f959eb29f5e6116bde939d1677862
Drew Keller <954172+drewkeller@users.noreply.github.com>
2020-09-26 11:13:06 -0500

    * Add WPF project

---
+++ Skippy.WPF/Interfaces/ScreenService.cs
@@ -0,0 +1,19 @@
+﻿using Skippy.Interfaces;
+using System;
+using Xamarin.Essentials;
+
+namespace Skippy.Interfaces
+{
+    public class ScreenService : IScreenService
+    {
+
+        public string ScreenshotFolder
+        {
+            get {
+                return Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                //return Path.GetFullPath(Path.Combine(FileSystem.AppDataDirectory, "..", "AppData"));
+            }
+        }
+
+    }
+}
